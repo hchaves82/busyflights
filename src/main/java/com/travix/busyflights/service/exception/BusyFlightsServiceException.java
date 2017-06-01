ffdfedcd5991839476cc0bbee642fdef735a7edc
Helcio Chaves <helciochaves82@gmail.com>
2017-06-01 17:46:39 +0100

    changing async call and creating loggerAspect

--- src/main/java/com/travix/busyflights/service/exception/BusyFlightsServiceException.java
+++
@@ -1,8 +0,0 @@
-package com.travix.busyflights.service.exception;
-
-public class BusyFlightsServiceException extends RuntimeException {
-
-	/**	 */
-	private static final long serialVersionUID = -3182464881769196515L;
-
-}
